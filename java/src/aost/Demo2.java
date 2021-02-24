bd5a8b19bb2150e002dae3457cafa93bb3475d5c
aost <1628590697@qq.com>
2021-02-25 01:28:48 +0800

    commit

--- java/src/aost/Demo2.java
+++ java/src/aost/Demo2.java
@@ -3,11 +3,99 @@ package aost;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 
 //斗地主案例实现
 public class Demo2 {
     public static void main(String[] args) {
+        //arr();
+        map();
+    }
 
+    private static void map() {
+        //牌库
+        String[] h = {"♠", "♣", "♥", "♦"};
+        String[] s = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
+
+
+        //创建索引
+        HashMap<Integer,String> paiMap=new HashMap<>();
+        ArrayList<Integer> integers=new ArrayList<>();
+        Integer in=0;
+
+        integers.add(in);
+        paiMap.put(in,"大王");
+        in++;
+        integers.add(in);
+        paiMap.put(in,"小王");
+        in++;
+
+        for (String s3 : s) {
+            for (String h1 : h) {
+                paiMap.put(in,h1+s3);
+                integers.add(in);
+                in++;
+            }
+        }
+
+
+        System.out.println(paiMap);
+
+
+        //洗牌
+        Collections.shuffle(integers);
+
+        //玩家
+        ArrayList<Integer> w1 = new ArrayList<>();
+        ArrayList<Integer> w2 = new ArrayList<>();
+        ArrayList<Integer> w3 = new ArrayList<>();
+        ArrayList<Integer> diPai = new ArrayList<>();
+
+
+        for (int i = 0; i < integers.size(); i++) {
+            Integer ins = integers.get(i);
+            if (i < 51) {
+                if (i % 3 == 0) {
+                    w1.add(ins);
+                } else if (i % 3 == 1) {
+                    w2.add(ins);
+                } else {
+                    w3.add(ins);
+                }
+            } else diPai.add(ins);
+        }
+
+        Collections.sort(w1);
+        Collections.sort(w2);
+        Collections.sort(w3);
+        Collections.sort(diPai);
+
+        System.out.println(w1);
+        System.out.println(w2);
+        System.out.println(w3);
+        System.out.println(diPai);
+
+//        String[] name={"周星驰","lbw","周润发","底牌"};
+        kanpai("周星驰",paiMap,w1);
+        kanpai("lbw",paiMap,w2);
+        kanpai("周润发",paiMap,w3);
+        kanpai("底牌",paiMap,diPai);
+
+
+    }
+
+    private static void kanpai(String name,HashMap<Integer,String> map,ArrayList<Integer> list) {
+        System.out.print(name+":");
+        for (Integer i :
+                list) {
+            System.out.print(map.get(i)+" ");
+        }
+        System.out.println();
+    }
+
+
+    private static void arr() {
         //牌库
         String[] h = {"♠", "♣", "♥", "♦"};
         String[] s = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
---
+++ java/src/aost/Study9.java
@@ -0,0 +1,13 @@
+package aost;
+
+
+//debug
+public class Study9 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=2;
+        System.out.println(a+b);
+        String s="qq";
+        System.out.println("qwq"+s);
+    }
+}
