4345b0b0797d4cde5f438ec240f559fcfd468186
Aost <1628590697@qq.com>
2021-03-26 21:00:56 +0800

    commit

---
+++ java/src/aost/Ams6.java
@@ -0,0 +1,19 @@
+package aost;
+
+public class Ams6 {
+    public static void main(String[] args) {
+
+    }
+    public Ams4.ListNode reverseKGroup (Ams4.ListNode head, int k) {
+        // write code here
+            if (head==null||head.next==null||k==1) return head;
+            int t=k-1;
+        Ams4.ListNode pre=head;
+            while (t!=0){
+                
+            }
+
+            return  null;
+    }
+
+}
