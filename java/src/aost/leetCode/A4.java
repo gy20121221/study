7c84d5f15d9ca3b0086acba2256fe3a01f8d2f41
aost <3398@winnermedical.com>
2021-07-21 09:17:02 +0800

    commit

--- java/src/aost/leetCode/A4.java
+++ java/src/aost/leetCode/A4.java
@@ -1,23 +1,31 @@
 package aost.leetCode;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author aost
  * @version 1.0
- * @description: 
+ * @description:
  * @date 2021/7/14 10:09
  */
 public class A4 {
     public static void main(String[] args) {
 
-//        System.out.println(sumNums(10));
-        int[] ints={1,2,3,4,5,6,7,8,8,9,4,5,3,5};
+        /*     System.out.println(sumNums(10));*/
 
-        System.out.println(findRepeatNumber(ints));
+       /*  int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 4, 5, 3, 5};
+        System.out.println(findRepeatNumber(ints));*/
+
+//        char[][] chars = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+//
+////        char[][] chars1 = {{'a', 'b'}};
+//
+//
+//        System.out.println(exist(chars, "ABCCED"));
+
+//        System.out.println(exist(chars1, "ba"));
+
+
     }
 
     /**
@@ -30,12 +38,12 @@ public class A4 {
 
     public static String minNumber(int[] nums) {
         String[] strs = new String[nums.length];
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             strs[i] = String.valueOf(nums[i]);
         }
         Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));
         StringBuilder res = new StringBuilder();
-        for(String s : strs) {
+        for (String s : strs) {
             res.append(s);
         }
         return res.toString();
@@ -48,12 +56,13 @@ public class A4 {
      * param []
      * return void
      */
-    static int[] test=new int[]{0};
+    static int[] test = new int[]{0};
+
     public static int sumNums(int n) {
-        try{
+        try {
             return test[n];
-        }catch(Exception e){
-            return n+sumNums(n-1);
+        } catch (Exception e) {
+            return n + sumNums(n - 1);
         }
     }
 
@@ -61,6 +70,7 @@ public class A4 {
     /**
      * description  在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
      * 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+     *
      * @author aost
      * date 16:43 2021/7/19
      * param [nums]
@@ -68,19 +78,21 @@ public class A4 {
      */
 
     public static int findRepeatNumber1(int[] nums) {
-        LinkedList<Integer> list=new LinkedList<>();
+        LinkedList<Integer> list = new LinkedList<>();
         list.add(nums[0]);
-            for (int i = 1; i < nums.length; i++) {
-                for (Integer integer : list) {
-                    if (integer==nums[i]){return nums[i];}
+        for (int i = 1; i < nums.length; i++) {
+            for (Integer integer : list) {
+                if (integer == nums[i]) {
+                    return nums[i];
                 }
-                list.add(nums[i]);
             }
+            list.add(nums[i]);
+        }
         return 0;
     }
 
     public static int findRepeatNumber(int[] nums) {
-        Set<Integer> set= new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             if (set.contains(num)) {
                 return num;
@@ -91,10 +103,10 @@ public class A4 {
     }
 
 
-
     /**
      * description   在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
      * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     *
      * @author aost
      * 0  time 6 9%  ram  99%
      * * 1  time 0 100%  ram  50%
@@ -104,23 +116,27 @@ public class A4 {
      */
 
     public boolean findNumberIn2dArray(int[][] matrix, int target) {
-       Set<Integer> set=new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int[] ints : matrix) {
             for (int anInt : ints) {
                 set.add(anInt);
-                if (set.contains(target)){ return true;}
+                if (set.contains(target)) {
+                    return true;
+                }
             }
         }
-       return false;
+        return false;
     }
 
 
     public boolean findNumberIn2dArray1(int[][] matrix, int target) {
-        int q=matrix.length-1,w=0;
-        while (q>=0&&w<matrix[0].length){
-            if (matrix[q][w]>target){q--;}
-            else if (matrix[q][w]<target){w++;}
-            else {
+        int q = matrix.length - 1, w = 0;
+        while (q >= 0 && w < matrix[0].length) {
+            if (matrix[q][w] > target) {
+                q--;
+            } else if (matrix[q][w] < target) {
+                w++;
+            } else {
                 return true;
             }
         }
@@ -128,10 +144,10 @@ public class A4 {
     }
 
 
-
     /**
      * description  一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n级的台阶总共有多少种跳法。
      * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     *
      * @author aost
      * time 0  100% ram 94%
      * date 11:09 2021/7/20
@@ -139,12 +155,14 @@ public class A4 {
      * return int
      */
     public int numWays(int n) {
-        if (n<=1) { return 1; }
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i = 2; i < n+1; i++){
-            dp[i] = (dp[i-1] + dp[i-2])%1000000007;
+        if (n <= 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
         }
         return dp[n];
     }
@@ -154,6 +172,7 @@ public class A4 {
      * description  把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
      * 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
      * 例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
+     *
      * @author aost
      * time 0  100%  ram  17%
      * date 11:25 2021/7/20
@@ -162,9 +181,11 @@ public class A4 {
      */
 
     public int minArray(int[] numbers) {
-        if (numbers.length<=1) {return numbers[0];}
+        if (numbers.length <= 1) {
+            return numbers[0];
+        }
         for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i-1]>numbers[i]) {
+            if (numbers[i - 1] > numbers[i]) {
                 return numbers[i];
             }
         }
@@ -172,7 +193,46 @@ public class A4 {
     }
 
 
+    /**
+     * description  给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false
+     * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+     * 同一个单元格内的字母不允许被重复使用。
+     *
+     * @author aost
+     * time 4 99%  arm  50%
+     * date 15:44 2021/7/20
+     * param [board, word]
+     * return boolean
+     */
 
+
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) {
+            return false;
+        }
+        if(k == word.length - 1) {
+            return true;
+        }
+        board[i][j] = '\0';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = word[k];
+        return res;
+    }
 
 
 
---
+++ java/src/aost/leetCode/A5.java
@@ -0,0 +1,37 @@
+package aost.leetCode;
+
+import java.util.ArrayList;
+
+/**
+ * @author aost
+ * @version 1.0
+ * @description:
+ * @date 2021/7/21 9:01
+ */
+public class A5 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    /**
+     * description  请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+     * @author aost
+     * time 0 100%  arm  43%
+     * date 9:02 2021/7/21
+     * param [s]
+     * return java.lang.String
+     */
+
+    public String replaceSpace(String s) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c==' ') {stringBuilder.append("%20");}
+            else  {stringBuilder.append(c);}
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
